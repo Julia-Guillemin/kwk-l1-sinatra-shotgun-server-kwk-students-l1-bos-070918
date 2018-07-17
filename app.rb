d2944831380ff28f0971ab6d319f4d7bdf83bff0
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-17 13:35:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Canada's giant duck is whats keeping me alive!!!! it even funyer that to pop and barkruped."
+    "Canada's giant duck is whats keeping me alive!!!! it even funnier that to pop and barkruped some of Canada."
   end
 
 end
