a474fa28f1dc8b61dff45ea3913c7809eea8d513
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-17 13:43:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,13 @@ class App < Sinatra::Base
     "Canada's giant duck is whats keeping me alive!!!! it is even funnier that it pop and barkruped some of Canada.! Sorry Justin."
   end
 
+gets '/duck' do 
+  "GIANT RUBBER DUCK RILLY"
+
 end
 
 # rack up will run but only once(edit ti then lonch)
+
 # shotgun will start your sanatra aplication and alow you to make changes then refresh to see an updated browser(edits in real time)
+
+# roots are something you just dd dient see stupo
