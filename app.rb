5f2fbf76aabbd68ec0cfb6c56b8c0f9aea5c68cc
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-17 13:30:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Canidas giant duck is whats keeping me alive!!!! "
+    "Canada's giant duck is whats keeping me alive!!!! "
   end
 
 end
+
+# rack up will run but only once(edit ti then lonch)
+# shotgun will start your sanatra aplication and alow you to make changes then refresh to see an updated browser(edits in real time)
